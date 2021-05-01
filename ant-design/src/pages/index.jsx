42aee1c497b4aa84f100fd0ab0975b80598d23a1
Alex <alex.karlsson.001@gmail.com>
2021-05-01 17:39:33 +0200

    Added Ant Design UI

--- ant-design/src/pages/index.jsx
+++ ant-design/src/pages/index.jsx
@@ -1,39 +1,184 @@
-import React from 'react'
-import { Collapse } from 'antd'
+import React, { Fragment, useState } from 'react'
+import {
+  Collapse,
+  Tabs,
+  Form,
+  Input,
+  Radio,
+  Checkbox,
+  Slider,
+  Select
+} from 'antd'
 
 const { Panel } = Collapse
+const { TabPane } = Tabs
+const { Option } = Select
 
+// This text is used in other components as seen in the Accordions and Tabs below
 const text = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Quisque gravida nibh ac augue ornare vestibulum. 
-Nunc eget porta nisl, vestibulum posuere ligula. 
-Nunc mollis blandit ante, sed placerat risus ornare ac. 
-Nunc tincidunt viverra tincidunt. Nullam sit amet massa nunc. 
-Vivamus nec risus vel risus scelerisque mattis eget at justo. 
-Ut tincidunt varius libero. 
-Donec porta justo et lacus molestie, ac rhoncus eros fermentum. 
-Nam sollicitudin blandit dolor et cursus. 
-Proin euismod eros sed fermentum varius. 
-Quisque vehicula egestas orci, ut pellentesque mauris lobortis vel. 
-Duis neque felis, dictum in laoreet sit amet, mollis quis nunc.
+Etiam elit orci, placerat non libero ac, dignissim consectetur tortor. 
+Etiam at rutrum ipsum, gravida maximus enim. 
+Donec eros lorem, pellentesque a magna quis, vestibulum eleifend ex. 
+In pretium auctor enim sit amet mollis. 
+Nulla risus eros, facilisis vel nulla quis, semper.
 `
 
+const accordionItems = [
+  { header: 'This is accordion header 1', text: text },
+  { header: 'This is accordion header 2', text: text + text },
+  { header: 'This is accordion header 3', text: text + text + text }
+]
+
+const tabsItems = [
+  { header: 'This is tab 1', text: text, disabled: false },
+  {
+    header: 'This is tab 2',
+    text: text + text,
+    disabled: false
+  },
+  { header: 'This is tab 3', text: null, disabled: true }
+]
+
+const inputs = [
+  { name: 'input-1', text: 'This is input 1', disbaled: false },
+  { name: 'input-2', text: 'This is input 2', disabled: true }
+]
+
+const checkboxes = [
+  {
+    name: 'check-1',
+    text: 'This is checkbox 1',
+    disabled: false
+  },
+  {
+    name: 'check-2',
+    text: 'This is checkbox 2',
+    disabled: true
+  }
+]
+
+const radios = [
+  {
+    name: 'radio-1',
+    text: 'This is radio 1',
+    disabled: false
+  },
+  {
+    name: 'radio-2',
+    text: 'This is radio 2',
+    disabled: false
+  },
+  {
+    name: 'radio-3',
+    text: 'This is radio 3',
+    disabled: true
+  }
+]
+
+const selectOptions = [
+  { value: 1, disabled: false },
+  { value: 2, disabled: true }
+]
+
+const sliders = [
+  { name: 'slider-1', text: 'This is slider 1', disabled: false },
+  { name: 'slider-2', text: 'This is slider 1', disabled: true }
+]
+
 const index = () => {
+  // State required to switch radios: https://ant.design/components/radio/
+  const [radioValue, setRadioValue] = useState(0)
+
+  const handleRadio = (event) => {
+    console.log(event.target.value)
+    setRadioValue(event.target.value)
+  }
+
   return (
     <main>
       <section className='component-section'>
+        {/* Accordion implementation: https://ant.design/components/collapse/ */}
         <article className='component-article'>
           <Collapse accordion>
-            <Panel header='This is panel header 1' key='1'>
-              <p>{text}</p>
-            </Panel>
-            <Panel header='This is panel header 2' key='2'>
-              <p>{text}</p>
-            </Panel>
-            <Panel header='This is panel header 3' key='3'>
-              <p>{text}</p>
-            </Panel>
+            {accordionItems.map((item, index) => {
+              return (
+                <Panel header={item.header} key={index}>
+                  <p>{item.text}</p>
+                </Panel>
+              )
+            })}
           </Collapse>
+        </article>
+        {/* Tabs implementation: https://ant.design/components/tabs/ */}
+        <article className='component-article'>
+          <Tabs defaultActiveKey='0'>
+            {tabsItems.map((item, index) => {
+              return (
+                <TabPane tab={item.header} key={index} disabled={item.disabled}>
+                  {item.text}
+                </TabPane>
+              )
+            })}
+          </Tabs>
+        </article>
+        {/* Form: https://ant.design/components/form/ */}
+        <article className='component-article'>
+          <Form>
+            {/* Inputs */}
+            {inputs.map((input, index) => {
+              return (
+                <Fragment key={index}>
+                  <Form.Item name={input.name} label={input.text}>
+                    <Input disabled={input.disabled} />
+                  </Form.Item>
+                </Fragment>
+              )
+            })}
+            {/* Checkbox */}
+            {checkboxes.map((checkbox, index) => {
+              return (
+                <Fragment key={index}>
+                  <Form.Item name={checkbox.name} label={checkbox.text}>
+                    <Checkbox value={index} disabled={checkbox.disabled} />
+                  </Form.Item>
+                </Fragment>
+              )
+            })}
+            {/* Radios */}
+            {radios.map((radio, index) => {
+              return (
+                <Radio.Group
+                  key={index}
+                  value={radioValue}
+                  onChange={handleRadio}
+                >
+                  {/* Does not work with <Form.Item>, tried to use it for label */}
+                  <Radio value={index} disabled={radio.disabled}>
+                    {radio.text}
+                  </Radio>
+                </Radio.Group>
+              )
+            })}
+            {/* Sliders */}
+            {sliders.map((slider, index) => {
+              return (
+                <Form.Item key={index} name={slider.name} label={slider.text}>
+                  <Slider disabled={slider.disabled}></Slider>
+                </Form.Item>
+              )
+            })}
+            {/* Select */}
+            <Form.Item name='select-1' label='Select'>
+              <Select defaultValue={selectOptions[0].value}>
+                {selectOptions.map((option, index) => (
+                  <Option key={index} value={option.value}>
+                    {option.value}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Form>
         </article>
       </section>
     </main>
