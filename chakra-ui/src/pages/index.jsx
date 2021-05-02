6d0e6a37c539da35a7a554f8fdfac6c790f8bbad
Alex <alex.karlsson.001@gmail.com>
2021-05-02 12:09:05 +0200

    Added Chakra UI

--- chakra-ui/src/pages/index.jsx
+++ chakra-ui/src/pages/index.jsx
@@ -1,62 +1,218 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Accordion,
   AccordionItem,
   AccordionButton,
   AccordionPanel,
   AccordionIcon,
-  Box
+  Box,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Checkbox,
+  CheckboxGroup,
+  Radio,
+  RadioGroup,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+  Select
 } from '@chakra-ui/react'
 
+// This text is used in other components as seen in the Accordions and Tabs below
 const text = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Quisque gravida nibh ac augue ornare vestibulum. 
-Nunc eget porta nisl, vestibulum posuere ligula. 
-Nunc mollis blandit ante, sed placerat risus ornare ac. 
-Nunc tincidunt viverra tincidunt. Nullam sit amet massa nunc. 
-Vivamus nec risus vel risus scelerisque mattis eget at justo. 
-Ut tincidunt varius libero. 
-Donec porta justo et lacus molestie, ac rhoncus eros fermentum. 
-Nam sollicitudin blandit dolor et cursus. 
-Proin euismod eros sed fermentum varius. 
-Quisque vehicula egestas orci, ut pellentesque mauris lobortis vel. 
-Duis neque felis, dictum in laoreet sit amet, mollis quis nunc.
+Etiam elit orci, placerat non libero ac, dignissim consectetur tortor. 
+Etiam at rutrum ipsum, gravida maximus enim. 
+Donec eros lorem, pellentesque a magna quis, vestibulum eleifend ex. 
+In pretium auctor enim sit amet mollis. 
+Nulla risus eros, facilisis vel nulla quis, semper.
 `
 
+const accordionItems = [
+  { header: 'This is accordion header 1', text: text },
+  { header: 'This is accordion header 2', text: text + text },
+  { header: 'This is accordion header 3', text: text + text + text }
+]
+
+const tabsItems = [
+  { header: 'This is tab 1', text: text, disabled: false },
+  {
+    header: 'This is tab 2',
+    text: text + text,
+    disabled: false
+  },
+  { header: 'This is tab 3', text: null, disabled: true }
+]
+
+const inputs = [
+  { name: 'input-1', text: 'This is input 1', disbaled: false },
+  { name: 'input-2', text: 'This is input 2', disabled: true }
+]
+
+const checkboxes = [
+  {
+    text: 'This is checkbox 1',
+    disabled: false
+  },
+  {
+    text: 'This is checkbox 2',
+    disabled: false
+  },
+  {
+    text: 'This is checkbox 3',
+    disabled: true
+  }
+]
+
+const radios = [
+  {
+    text: 'This is radio 1',
+    disabled: false
+  },
+  {
+    text: 'This is radio 2',
+    disabled: false
+  },
+  {
+    text: 'This is radio 3',
+    disabled: true
+  }
+]
+
+const selectOptions = [
+  { value: 1, disabled: false },
+  { value: 2, disabled: false },
+  { value: 3, disabled: true }
+]
+
+const sliders = [
+  { name: 'slider-1', text: 'This is slider 1', disabled: false },
+  { name: 'slider-2', text: 'This is slider 1', disabled: true }
+]
+
 const index = () => {
+  // State required to switch radios: https://chakra-ui.com/docs/form/radio
+  const [radioValue, setRadioValue] = useState(0)
   return (
     <main>
       <section className='component-section'>
+        {/* Accordion implementation: https://chakra-ui.com/docs/disclosure/accordion */}
         <article className='component-article'>
           <Accordion>
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex='1' textAlign='left'>
-                    Section 1 title
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                {text}
-              </AccordionPanel>
-            </AccordionItem>
-
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex='1' textAlign='left'>
-                    Section 2 title
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                {text}
-              </AccordionPanel>
-            </AccordionItem>
+            {accordionItems.map((item, index) => {
+              return (
+                <AccordionItem key={index}>
+                  <h2>
+                    <AccordionButton>
+                      <Box flex='1' textAlign='left'>
+                        {item.header}
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>{item.text}</AccordionPanel>
+                </AccordionItem>
+              )
+            })}
           </Accordion>
+        </article>
+        {/* Tabs implementation: https://chakra-ui.com/docs/disclosure/tabs */}
+        <article className='component-article'>
+          <Tabs>
+            <TabList>
+              {tabsItems.map((item, index) => (
+                <Tab key={index} isDisabled={item.disabled}>
+                  {item.header}
+                </Tab>
+              ))}
+            </TabList>
+            <TabPanels>
+              {tabsItems.map((item, index) => (
+                <TabPanel key={index}>{item.text}</TabPanel>
+              ))}
+            </TabPanels>
+          </Tabs>
+        </article>
+        {/* Form implementation: https://chakra-ui.com/docs/form/form-control */}
+        <article className='component-article'>
+          <form>
+            {/* Input: https://chakra-ui.com/docs/form/input and Form Control for label: https://chakra-ui.com/docs/form/form-control */}
+            {inputs.map((input, index) => {
+              return (
+                <FormControl key={index} id={input.name} isDisabled={input}>
+                  <FormLabel>{input.text}</FormLabel>
+                  <Input />
+                </FormControl>
+              )
+            })}
+            {/* Checkbox (uncontrolled, no value): https://chakra-ui.com/docs/form/checkbox */}
+            {/* Stack seems to be standard to use for stacking/spacing */}
+            <CheckboxGroup>
+              <Stack direction='row'>
+                {checkboxes.map((checkbox, index) => (
+                  <Checkbox key={index} isDisabled={checkbox.disabled}>
+                    {checkbox.text}
+                  </Checkbox>
+                ))}
+              </Stack>
+            </CheckboxGroup>
+            {/* Radio: https://chakra-ui.com/docs/form/form-control */}
+            {/* Stack seems to be standard to use for stacking/spacing */}
+            <RadioGroup
+              value={Number(radioValue)}
+              onChange={(radioValue) => setRadioValue(radioValue)}
+            >
+              <Stack direction='row'>
+                {radios.map((radio, index) => (
+                  <Radio
+                    key={index}
+                    value={Number(index)}
+                    isDisabled={radio.disabled}
+                  >
+                    {radio.text}
+                  </Radio>
+                ))}
+              </Stack>
+            </RadioGroup>
+            {/* Slider: https://chakra-ui.com/docs/form/slider */}
+            {/* Form Control for label: https://chakra-ui.com/docs/form/form-control (no other method to include text/label in the component documentation) */}
+            {/* Docs recommended aria-label */}
+            {sliders.map((slider, index) => (
+              <FormControl key={index} id={slider.name}>
+                <FormLabel>{slider.text}</FormLabel>
+                <Slider isDisabled={slider.disabled}>
+                  <SliderTrack aria-label={slider.name}>
+                    <SliderFilledTrack />
+                  </SliderTrack>
+                  <SliderThumb />
+                </Slider>
+              </FormControl>
+            ))}
+            {/* Select: */}
+            {/* Form Control for label: https://chakra-ui.com/docs/form/form-control (no other method to include text/label in the component documentation) */}
+            <FormControl id='select-1'>
+              <FormLabel>Select</FormLabel>
+              <Select defaultValue={selectOptions[0].value}>
+                {selectOptions.map((option, index) => (
+                  <option
+                    key={index}
+                    disabled={option.disabled}
+                    value={option.value}
+                  >
+                    {option.value}
+                  </option>
+                ))}
+              </Select>
+            </FormControl>
+          </form>
         </article>
       </section>
     </main>
