ea6a3e83c2933f6b4648b61a08156e0251a6e1c5
Alex <alex.karlsson.001@gmail.com>
2021-05-03 13:42:20 +0200

    Minor refactor

--- react-bootstrap/src/pages/index.jsx
+++ react-bootstrap/src/pages/index.jsx
@@ -29,50 +29,50 @@ const tabsItems = [
 ]
 
 const inputs = [
-  { text: 'This is input 1', controlId: 'inputControl1', disbaled: false },
-  { text: 'This is input 2', controlId: 'inputControl2', disabled: true }
+  { text: 'This is input 1', controlId: 'input-id-1', disbaled: false },
+  { text: 'This is input 2', controlId: 'input-id-2', disabled: true }
 ]
 
 const checkboxes = [
   {
-    name: 'check-1',
-    id: 'checkbox-1',
+    name: 'checkbox-1-name',
+    id: 'checkbox-1-id',
     text: 'This is checkbox 1',
     disabled: false
   },
   {
-    name: 'check-2',
-    id: 'checkbox-2',
+    name: 'checkbox-2-name',
+    id: 'checkbox-2-id',
     text: 'This is checkbox 2',
+    disabled: false
+  },
+  {
+    name: 'checkbox-3-name',
+    id: 'checkbox-3-id',
+    text: 'This is checkbox 3',
     disabled: true
   }
 ]
 
 const radios = [
   {
-    name: 'radio-1',
-    id: 'radio-1',
+    name: 'radio-1-name',
+    id: 'radio-1-id',
     text: 'This is radio 1',
     disabled: false
   },
   {
-    name: 'radio-2',
-    id: 'radio-2',
+    name: 'radio-2-name',
+    id: 'radio-2-id',
     text: 'This is radio 2',
     disabled: false
   },
   {
-    name: 'radio-3',
-    id: 'radio-3',
+    name: 'radio-3-name',
+    id: 'radio-3-id',
     text: 'This is radio 3',
     disabled: true
   }
-]
-
-const selectOptions = [
-  { value: 1, disabled: false },
-  { value: 2, disabled: false },
-  { value: 3, disabled: true }
 ]
 
 const sliders = [
@@ -80,36 +80,40 @@ const sliders = [
   { id: 'slider-2', label: 'This is slider 2', disabled: true }
 ]
 
+const selectOptions = [
+  { value: 0, disabled: false },
+  { value: 1, disabled: false },
+  { value: 2, disabled: true }
+]
+
 const index = () => {
   return (
     <main>
       <section className='component-section'>
-        {/* Accordion implementation: https://react-bootstrap.github.io/components/accordion/ */}
+        {/* Accordion: https://react-bootstrap.github.io/components/accordion/ */}
         <article className='component-article'>
           <Accordion>
-            {accordionItems.map((item, index) => {
-              return (
-                <div key={index}>
-                  <Card>
-                    <Card.Header>
-                      <Accordion.Toggle
-                        as={Button}
-                        variant='link'
-                        eventKey={`${index}`}
-                      >
-                        {item.header}
-                      </Accordion.Toggle>
-                    </Card.Header>
-                    <Accordion.Collapse eventKey={`${index}`}>
-                      <Card.Body>{item.text}</Card.Body>
-                    </Accordion.Collapse>
-                  </Card>
-                </div>
-              )
-            })}
+            {accordionItems.map((item, index) => (
+              <div key={index}>
+                <Card>
+                  <Card.Header>
+                    <Accordion.Toggle
+                      as={Button}
+                      variant='link'
+                      eventKey={`${index}`}
+                    >
+                      {item.header}
+                    </Accordion.Toggle>
+                  </Card.Header>
+                  <Accordion.Collapse eventKey={`${index}`}>
+                    <Card.Body>{item.text}</Card.Body>
+                  </Accordion.Collapse>
+                </Card>
+              </div>
+            ))}
           </Accordion>
         </article>
-        {/* Tab implementation: https://react-bootstrap.github.io/components/tabsItems/ */}
+        {/* Tab: https://react-bootstrap.github.io/components/tabsItems/ */}
         {/* Note: Abstract class, unable to use map() */}
         <article className='component-article'>
           <Tabs
@@ -139,66 +143,54 @@ const index = () => {
             </Tab>
           </Tabs>
         </article>
-        {/* Form implementation: https://react-bootstrap.github.io/components/forms/ */}
+        {/* Form: https://react-bootstrap.github.io/components/forms/ */}
         <article className='component-article'>
           <Form>
             {/* Input */}
-            {inputs.map((input, index) => {
-              return (
-                <div key={index}>
-                  <Form.Group controlId={input.controlId}>
-                    <Form.Label>{input.text}</Form.Label>
-                    <Form.Control
-                      type='text'
-                      disabled={input.disabled}
-                    ></Form.Control>
-                  </Form.Group>
-                </div>
-              )
-            })}
+            {inputs.map((input, index) => (
+              <Form.Group key={index} controlId={input.controlId}>
+                <Form.Label>{input.text}</Form.Label>
+                <Form.Control
+                  type='text'
+                  disabled={input.disabled}
+                ></Form.Control>
+              </Form.Group>
+            ))}
             {/* Checkbox */}
             <Form.Group controlId='checkboxControl'>
-              {checkboxes.map((checkbox, index) => {
-                return (
-                  <div key={index}>
-                    <Form.Check
-                      id={checkbox.id}
-                      name={checkbox.name}
-                      label={checkbox.text}
-                      disabled={checkbox.disabled}
-                    />
-                  </div>
-                )
-              })}
+              {checkboxes.map((checkbox, index) => (
+                <Form.Check
+                  key={index}
+                  id={checkbox.id}
+                  name={checkbox.name}
+                  label={checkbox.text}
+                  disabled={checkbox.disabled}
+                />
+              ))}
             </Form.Group>
             {/* Radio */}
             <Form.Group controlId='radioControl'>
-              {radios.map((radio, index) => {
-                return (
-                  <div key={index}>
-                    <Form.Check
-                      id={radio.id}
-                      name={radio.name}
-                      label={radio.text}
-                      disabled={radio.disabled}
-                    />
-                  </div>
-                )
-              })}
+              {radios.map((radio, index) => (
+                <Form.Check
+                  key={index}
+                  id={radio.id}
+                  name={radio.name}
+                  label={radio.text}
+                  disabled={radio.disabled}
+                />
+              ))}
             </Form.Group>
             {/* Slider */}
             <Form.Group controlId='formBasicRangeCustom'>
-              {sliders.map((slider) => {
-                return (
-                  <div key={index}>
-                    <Form.Label>{slider.text}</Form.Label>
-                    <Form.Control
-                      type='range'
-                      disabled={slider.disabled}
-                    ></Form.Control>
-                  </div>
-                )
-              })}
+              {sliders.map((slider) => (
+                <div key={index}>
+                  <Form.Label>{slider.text}</Form.Label>
+                  <Form.Control
+                    type='range'
+                    disabled={slider.disabled}
+                  ></Form.Control>
+                </div>
+              ))}
             </Form.Group>
             {/* Select */}
             <Form.Group controlId='selectControl'>
